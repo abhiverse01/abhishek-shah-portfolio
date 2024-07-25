f81ffa0c5f3efcc42bdb3a8627da318094b02e7a
abhiverse01 <www.abhishekshah007@gmail.com>
2024-07-25 16:29:11 +0545

    loding animation js action

--- assets/js/main.js
+++ assets/js/main.js
@@ -55,3 +55,15 @@ sr.reveal('.home__data, .about__img, .skills__subtitle, .skills__text',{});
 sr.reveal('.home__img, .about__subtitle, .about__text, .skills__img',{delay: 0}); 
 sr.reveal('.home__social-icon',{ interval: 0}); 
 sr.reveal('.skills__data, .work__img, .contact__input',{interval: 0}); 
+
+/*===== SKILLS BAR ANIMATION =====*/
+document.addEventListener('DOMContentLoaded', () => {
+    const bars = document.querySelectorAll('.skills__bar');
+    bars.forEach(bar => {
+        const width = bar.getAttribute('style').split(': ')[1];
+        bar.style.width = '0';
+        setTimeout(() => {
+            bar.style.width = width;
+        }, 100);
+    });
+});
